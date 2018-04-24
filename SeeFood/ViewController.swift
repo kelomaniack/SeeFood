ccc361e336283ef04316af6f6b705c36320317d6
Evangelos Konstantinidis <evangelos.konstantinidis@exxeta.com>
2018-04-24 23:20:38 +0200

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Konstantinidis, Evangelos on 24.04.18.
+//  Copyright © 2018 Konstantinidis, Evangelos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
